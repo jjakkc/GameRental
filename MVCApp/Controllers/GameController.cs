c0423d81bd065ca3b551b1170b9e1f32681b21cd
jjakkc <7483411+jjakkc@users.noreply.github.com>
2020-12-17 01:56:08 -0500

    Add project files.

---
+++ MVCApp/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApp.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (string.IsNullOrEmpty(sortBy))
+                sortBy = "Release date";
+
+            return Content($"pageIndex={pageIndex} sortBy={sortBy}");
+        }
+
+        public ActionResult Random(int contentID)
+        {
+            return Content(String.Format("Id={0}", contentID));
+        }
+    }
+}
